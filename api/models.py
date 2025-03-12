c966477b65d6f813fa0770dbe73b1178c4bc9fd6
peleisaac <151077839+peleisaac@users.noreply.github.com>
2025-03-12 13:47:33 +0000

    Updated models.py with new fields for UsersModel

--- api/models.py
+++ api/models.py
@@ -23,6 +23,11 @@ class Users(AbstractBaseUser):
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
     resume_url = models.CharField(max_length=200, blank=True, null=True)
+    company_name = models.CharField(max_length=200, blank=True, null=True)
+    contact_name = models.CharField(max_length=200, blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    industry = models.CharField(max_length=200, blank=True, null=True)
+    company_description = models.TextField(blank=True, null=True)
     msisdn = models.CharField(max_length=10, unique=True, blank=True, null=True)  # Make msisdn unique
     gender = models.CharField(max_length=10, blank=True, null=True)
     user_role = models.CharField(max_length=50)
@@ -98,6 +103,12 @@ class Users(AbstractBaseUser):
             "socials": user.socials,
             "user_role": user.user_role,
             'resume_url': user.resume_url,
+            "is_active": user.is_active,
+            "company_name": user.company_name,
+            "contact_name": user.contact_name,
+            "address": user.address,
+            "industry": user.industry,
+            "company_description": user.company_description,
             "category_of_interest": json.loads(user.category_of_interest),
             "job_notifications": user.job_notifications
          } if user else None
